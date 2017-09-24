5317b4d7b87d319c808ea5e3470dfa13432b1001
loganmurphy <loganmurphy1984@gmail.com>
2017-09-24 17:37:57 -0500

    updated 9/24

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,43 @@
-secret_number = 5
+import random
 
-while True:
+print("I'm thinking of a number between 1 and 10.")
+secret_number = random.randint(1, 11)
+guesses = 5
+
+while guesses > 0:
     guess = input("What is the secret number? ")
     if(int(guess) == secret_number):
+        print("That's right, you're a genius!")
         break
+    elif(int(guess) > secret_number):
+        print(guess, "is too high.")
+        guesses -= 1
+        if guesses > 1:
+            print("Your have", guesses, "guesses left.")
+        elif guesses == 1:
+            print("Your have", guesses, "guess left.")
+        else:
+            print("Sorry, you're all out of guesses.")
+            play_again = input("Do you want to play again? ")
+            if play_again.lower() == "yes":
+                guesses += 5
+                secret_number = random.randint(1, 11)
+            else:
+                print("Goodbye.")
+                break
     else:
-        print("Guess again!")
-
-print("That's right, you're a genius!")
+        print(guess, "is too low.")
+        guesses -= 1
+        if guesses > 1:
+            print("Your have", guesses, "guesses left.")
+        elif guesses == 1:
+            print("Your have", guesses, "guess left.")
+        else:
+            print("Sorry, you're all out of guesses.")
+            play_again = input("Do you want to play again? ")
+            if play_again.lower() == "yes":
+                guesses += 5
+                secret_number = random.randint(1, 11)
+            else:
+                print("Goodbye.")
+                break
--- guess_number_pt2.py
+++ guess_number_pt2.py
@@ -1,19 +1,20 @@
 import random
 
 secret_number = int(input("Pick a number between 1 and 100. "))
-computer_guess = 0
+computer_guess = 50
 tries = 0
 
 while True:
     if(computer_guess != secret_number):
-        computer_guess = (random.randrange(0, 100, 1))
         tries += 1
         print(computer_guess)
-        input("Is this your number? ")
-    else:
-        print(computer_guess)
-        input("Is this your number? ")
-        break
+        answer = input("Is this your number? ")
+        if answer.lower() == "too high":
+            computer_guess -= 1
+        elif answer.lower() == "too low":
+            computer_guess += 1
+        else:
+            break
 
 print("Your number is ", secret_number)
 print("It took the computer {} tries.".format(tries))
